57ab1543f058362b615e9c128847278d321424a5
Debatreya <debatreyadas@gmail.com>
2024-03-18 01:49:21 +0530

    Serving Static Files

--- server/app.js
+++ server/app.js
@@ -1,15 +1,21 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 config();
-import cookieParser from 'cookie-parser';
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import userRoutes from './routes/user.routes.js'
-import courseRoutes from './routes/course.routes.js'
-import paymentRoutes from './routes/payment.routes.js'
-import misRoutes from './routes/miscellaneous.routes.js'
-import youtubeRoutes from './routes/scrapped.routes.js'
-import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+import morgan from "morgan";
+import userRoutes from "./routes/user.routes.js";
+import courseRoutes from "./routes/course.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
+import misRoutes from "./routes/miscellaneous.routes.js";
+import youtubeRoutes from "./routes/scrapped.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
@@ -17,33 +23,34 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(morgan('tiny'));
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    credentials: true
-}));
+app.use(morgan("tiny"));
+app.use(
+	cors({
+		origin: [process.env.FRONTEND_URL],
+		credentials: true,
+	})
+);
 
 app.use(cookieParser());
 
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
 // routes test
-app.get('/', (req, res) => {
-    return res.send('Hello From Bookmark Server');
-})
+app.use("/ping", function (req, res) {
+	res.send("Pong");
+});
 
-app.use('/ping', function(req, res){
-    res.send('Pong');
-})
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/courses", courseRoutes);
+app.use("/api/v1/payments", paymentRoutes);
+app.use("/api/v1", misRoutes);
+app.use("/api/v1/youtube", youtubeRoutes);
 
-app.use('/api/v1/user', userRoutes)
-app.use('/api/v1/courses', courseRoutes);
-app.use('/api/v1/payments', paymentRoutes);
-app.use('/api/v1', misRoutes);
-app.use('/api/v1/youtube', youtubeRoutes);
-
-// Other BAD Requests
-app.all('*', (req, res)=>{
-    res.status(404).send('OOPS!! 404 page not found');
-})
+// serves index.html
+app.get("*", (req, res) => {
+	// from here the react app takes over
+	res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+});
 
 app.use(errorMiddleware);
 
