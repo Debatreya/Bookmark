1a8c7a591e766cc9a37e999fd6c258ef1edf7617
Debatreya <debatreyadas@gmail.com>
2024-03-18 02:13:34 +0530

    To serve static frontend

--- client/src/helpers/axiosinstance.js
+++ client/src/helpers/axiosinstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.VITE_SERVER}/api/v1`
+const BASE_URL = `${import.meta.env.VITE_SERVER}/api/v1` || '/api/v1'
+console.log(BASE_URL);
 
 const axiosInstance = axios.create();
 
